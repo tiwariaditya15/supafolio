0954b79f04dd769e0f076afd4265585ce6dd9220
Aditya Tiwari <tiwariaditya15@hotmail.com>
2022-01-31 20:24:18 +0530

    feat: redirect if logged

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,15 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import LogIn from "../components/LogIn";
 import supabase from "../utils/supabase";
+
 const Login = () => {
+  const router = useRouter();
+  useEffect(() => {
+    if (supabase.auth.user()) {
+      router.push("/");
+    }
+  }, []);
   return <LogIn />;
 };
 
